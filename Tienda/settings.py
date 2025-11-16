a28a45d5e31a22a6b7abc8d2a6eea3d9a467e7e1
Leandro Sosa <sosadariocanteroleadro@gmail.com>
2025-11-16 07:52:03 -0300

    Regresando a la version actual debido a problemas en el avanze del proyecto

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'Tienda.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'Pagina'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
